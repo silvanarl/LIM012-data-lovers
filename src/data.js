725a32caced46946a2c3ceb6a5dd9f4635552781
silvanarl <ramossilvana495@gmail.com>
2020-03-10 13:26:28 -0500

    primera subida

--- src/data.js
+++ src/data.js
@@ -1,5 +1,9 @@
 // estas funciones son de ejemplo
 
 export const example = () => 'example';
-
+// function cargarImagen(){    
+//     let imagen = document.getElementById("root");    
+//     document.imagen.src = pokemon[0].img;
+// }
+// cargarImagen();
 export const anotherExample = () => 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 
+console.log(pokemon.length);
 console.log(example, data);
