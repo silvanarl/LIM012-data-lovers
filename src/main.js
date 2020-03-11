d790b6fbf87ccd872c0d224a4c0b630d8b1708cf
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-03-11 13:19:24 -0500

    subí el 11mar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import pokemon from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+const listado = pokemon;
+console.log(listado.pokemon[1].name);
+console.log(listado.pokemon[1].type);
+
+document.body.onload = addElement;
+
+function addElement(){
+    const newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'contenedorPokemon')
+    //newDiv.classList.add('contenedorPokemon');
+
+    const newContent = document.createTextNode('Hola de nuevo');
+    newDiv.appendChild(newContent);
+
+    let currentDiv = document.getElementById('contenedor');
+    currentDiv.appendChild(newDiv);
+}
+
