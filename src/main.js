b0489d99d785c9dfbdb8cab25f188a37927e3419
silvanarl <ramossilvana495@gmail.com>
2020-03-15 01:13:47 -0500

    solo un filter

--- src/main.js
+++ src/main.js
@@ -2,33 +2,46 @@ import { example } from './data.js';
 import pokemon from './data/pokemon/pokemon.js';
 
 const listado = pokemon;
-//console.log(listado.pokemon[1].name);
-//console.log(listado.pokemon[1].type);
+// console.log(listado.pokemon[1].name);
+// console.log(listado.pokemon[1].type);
 
-//console.log(Object.keys(listado.pokemon));
+// console.log(Object.keys(listado.pokemon));
 
-listado.pokemon.forEach((element, index)=>{
+listado.pokemon.forEach((element, index) => {
 
-    const newDiv = document.createElement('div');
-    newDiv.setAttribute('class', 'contenedorPokemon');
+  const newDiv = document.createElement('div');
+  newDiv.setAttribute('class', 'contenedorPokemon');
 
-    // img
-    const newImg = document.createElement('img');
-    newImg.setAttribute("src", listado.pokemon[index].img);
-    newImg.setAttribute('class', 'imgPokemon');
+  // img
+  const newImg = document.createElement('img');
+  newImg.setAttribute('src', listado.pokemon[index].img);
+  newImg.setAttribute('class', 'imgPokemon');
 
-    //name
-    const newName = document.createTextNode(listado.pokemon[index].name);
+  // name
+  const newName = document.createTextNode(listado.pokemon[index].name);
 
-    //numero
-    const newNumber = document.createTextNode(listado.pokemon[index].num);
+  // numero
+  const newNumber = document.createTextNode(listado.pokemon[index].num);
 
-    newDiv.appendChild(newNumber);
-    newDiv.appendChild(newImg);
-    newDiv.appendChild(newName);
-    
+  newDiv.appendChild(newNumber);
+  newDiv.appendChild(newImg);
+  newDiv.appendChild(newName);
+
   const currentDiv = document.getElementById('contenedor');
   currentDiv.appendChild(newDiv);
 });
-//const ingresoNombre = document.getElementById('buscador').value
-//para poder hacer el merge
+// const ingresoNombre = document.getElementById('buscador').value
+// para poder hacer el merge
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('input[type=text]').forEach(node => node.addEventListener('keypress', e => {
+    if (e.keyCode == 13) {
+      e.preventDefault();
+    }
+  }));
+});
+const pokemonBuscado = prompt('¿Qué pokemon buscas?');
+console.log(pokemonBuscado);
+
+const pokemonParecido = listado.pokemon.filter(pokemon => (pokemon.name === pokemonBuscado));
+console.log(pokemonParecido);
