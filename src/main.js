8101a905ef53dad186685da86c70f9802d2f8267
silvanarl <ramossilvana495@gmail.com>
2020-03-16 01:07:17 -0500

    subiendo algo del buscador

--- src/main.js
+++ src/main.js
@@ -2,24 +2,17 @@ import { example } from './data.js';
 import pokemon from './data/pokemon/pokemon.js';
 
 const listado = pokemon;
-// console.log(listado.pokemon[1].name);
-// console.log(listado.pokemon[1].type);
-
-// console.log(Object.keys(listado.pokemon));
 
 listado.pokemon.forEach((element, index) => {
 
   const newDiv = document.createElement('div');
   newDiv.setAttribute('class', 'contenedorPokemon');
-
   // img
   const newImg = document.createElement('img');
   newImg.setAttribute('src', listado.pokemon[index].img);
   newImg.setAttribute('class', 'imgPokemon');
-
   // name
   const newName = document.createTextNode(listado.pokemon[index].name);
-
   // numero
   const newNumber = document.createTextNode(listado.pokemon[index].num);
 
@@ -30,18 +23,15 @@ listado.pokemon.forEach((element, index) => {
   const currentDiv = document.getElementById('contenedor');
   currentDiv.appendChild(newDiv);
 });
-// const ingresoNombre = document.getElementById('buscador').value
-// para poder hacer el merge
+const buscarPokemon = document.getElementById('botonBuscar');
+buscarPokemon.addEventListener('click', (validacion) => {
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('input[type=text]').forEach(node => node.addEventListener('keypress', e => {
-    if (e.keyCode == 13) {
-      e.preventDefault();
-    }
-  }));
+  validacion.preventDefault();
+  const pokemonBuscado = document.getElementById('buscador').value;
+  console.log(pokemonBuscado); // guarda el valor ingresado
+  const pokemonEncontrado = listado.pokemon.filter(pokemon => (pokemon.name === pokemonBuscado));
+  console.log(pokemonEncontrado); // Esto trae toda la info del pokemon
+  const soloTresPropiedades = pokemonEncontrado.map(pokemon => [[pokemon.num], [pokemon.img], [pokemon.name]]);
+  console.log(soloTresPropiedades);
+  //const nuevoDivEnPantalla;
 });
-const pokemonBuscado = prompt('¿Qué pokemon buscas?');
-console.log(pokemonBuscado);
-
-const pokemonParecido = listado.pokemon.filter(pokemon => (pokemon.name === pokemonBuscado));
-console.log(pokemonParecido);
