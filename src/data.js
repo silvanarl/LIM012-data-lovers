2d10070e70e68d4cc87bdf2328294217323651e4
silvanarl <ramossilvana495@gmail.com>
2020-03-20 12:21:41 -0500

    se arreglo las coincidencias

--- src/data.js
+++ src/data.js
@@ -1,5 +1,14 @@
 // estas funciones son de ejemplo
 
-export const example = () => 'example';
+export const coincidencias = (array, string) => {
+  const arrCoincidencias = [];
+  for (let i = 0; i < array.length; i++) {
+    const pokemonMin = array[i].name.toLowerCase();
+    if (pokemonMin.startsWith(string)) {
+      arrCoincidencias.push(array[i]);
+    }
+  }
+  return arrCoincidencias;
+};
 
 export const anotherExample = () => 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { example } from './data.js';
+import { coincidencias } from './data.js';
 import pokemon from './data/pokemon/pokemon.js';
 
 const arrObj = pokemon.pokemon;
@@ -31,32 +31,19 @@ const showPokemon = (array) => {
 };
 showPokemon(arrObj);
 
-// Esta función arroja coincidencias según el ingreso del usuario
-
-const pokemonBuscado = document.getElementById('buscador');
 const buscarPokemon = document.getElementById('botonBuscar');
-const arrCoincidencias = [];
-
-const coincidencias = (array) => {
-  const textoMin = pokemonBuscado.value.toLowerCase();
-  for (let i = 0; i < array.length; i++) {
-    const pokemonMin = array[i].name.toLowerCase();
-    if (pokemonMin.indexOf(textoMin) !== -1) {
-      arrCoincidencias.push(array[i]);
-    } else {
-    //   const textoAlerta = document.getElementById('textoAlerta');
-    //   textoAlerta.classList.remove('ocultar');
-    //   textoAlerta.textContent = 'no hay coincidencias';
-    }
-  }
-  return arrCoincidencias;
-};
 
 buscarPokemon.addEventListener('click', (event) => {
   event.preventDefault();
-  currentDiv.innerHTML = '';
-  showPokemon(coincidencias(arrObj));
-  pokemonBuscado.value = ''; //AGREGUÉ
+  let pokemonBuscado = document.getElementById('buscador').value;
+  if (pokemonBuscado !== '') {
+    console.log(pokemonBuscado);
+    currentDiv.innerHTML = '';
+    showPokemon(coincidencias(arrObj, pokemonBuscado));
+    pokemonBuscado = '';
+  } else {
+    alert('No se encontraron coincidencias');
+  }
 });
 
 // Mostrar la pokedex con la info del pokemon
@@ -64,10 +51,31 @@ buscarPokemon.addEventListener('click', (event) => {
 const btnInfo = document.querySelector('#btnInfo');
 const pokedex = document.querySelector('#overlay');
 
-const showInfo = () => {
+const showInfo = (array) => {
   let infoPok = '';
 
-  //for (let i = 0; i < array.length; i++)
+  for (let i = 0; i < array.length; i++) {
+    const num = array[i].num;
+    const img = array[i].img;
+    const name = array[i].name;
+
+    infoPok += `
+    <div class="pokedex" id="popup">
+      <div class="pok_1"></div>
+      <div class="pok_2"></div>
+      <div class="pok_3">
+        <div class="pok_3_1"></div>
+        <div class="pok_3_2"></div>
+      </div>
+      <div class="pok_4"></div>
+      <div class="info">
+      <p> ${name}</p>
+      <p> ${num}</p>
+      <img class = "imgPokemon" src = ${img}>
+      </div>
+    </div>
+    `;
+  }
     // const num = array[i].num;
     // const img = array[i].img;
     // const name = array[i].name;
@@ -81,21 +89,9 @@ const showInfo = () => {
     // const weaknesses = array[i].weaknesses;
     // const evolution = array[i].evolution[i].next-evolution[name];
 
-    infoPok += `
-    <div class="pokedex" id="popup">
-      <div class="pok_1"></div>
-      <div class="pok_2"></div>
-      <div class="pok_3">
-        <div class="pok_3_1"></div>
-        <div class="pok_3_2"></div>
-      </div>
-      <div class="pok_4"></div>
-      <div class="info">HOLA</div>
-    </div>
-    `;
   pokedex.innerHTML = infoPok;
 };
-showInfo();
+showInfo(arrObj);
 
 btnInfo.addEventListener('click', (event) => {
 
@@ -103,6 +99,5 @@ btnInfo.addEventListener('click', (event) => {
   //const popup = document.getElementById('popup');
 
   pokedex.classList.add('mostrar');
-  
   showInfo();
 });
