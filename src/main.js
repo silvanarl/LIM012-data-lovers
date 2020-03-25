46a128b1ef65f872aba8f8e42743b539a4ec338c
silvanarl <ramossilvana495@gmail.com>
2020-03-25 11:40:21 -0500

    resolviendo cambios

--- src/main.js
+++ src/main.js
@@ -42,23 +42,23 @@ const showInfo = (elemento) => {
         </div>
         <div class="info">
           <div class="nameAndNum">
-            <p class="text24"> ${name[0].toUpperCase()}${name.substring(1)} </p>
-            <p class="text24"> ${num} </p>
+            <p class="text1"> ${name[0].toUpperCase()}${name.substring(1)} </p>
+            <p class="text1"> ${num} </p>
           </div>
           <div class="secondRow">
             <img class = "imgPokInfo" src = ${img}>
             <div class="moreInfo">
               <div class="weight">
                 <img class="icono" src = "img/weight.svg">
-                <p class="text18"> ${weight} </p>
+                <p class="text2"> ${weight} </p>
               </div>
               <div class="height">
                 <img class="icono" src = "img/height.svg">
-                <p class="text18"> ${height} </p>
+                <p class="text2"> ${height} </p>
               </div>
               <div class="generation">
                 <img class="region" src = "img/pointer.svg">
-                <span class="text18"> ${generation[0].toUpperCase()}${generation.substring(1)} </span>
+                <span class="text2"> ${generation[0].toUpperCase()}${generation.substring(1)} </span>
               </div>
               <div class="type">         
                 <div class="separar">${dividir(type)}</div>
@@ -66,19 +66,20 @@ const showInfo = (elemento) => {
             </div>
           </div>
           <div class="thirdRow">
-            <p class="text18">Máx</p>
-            <img class = "icono" src = "img/glove.svg">
-            <p class="text18"> ${maxCp} </p>
-            <img class = "icono" src = "img/heart.svg">
-            <p class="text18"> ${maxHp} </p>
+            <p class="text2">Máx</p>
+            <img class="icono2" src = "img/glove.svg">
+            <p class="text2"> ${maxCp} </p>
+            <p class="text2">|</p>
+            <img class="icono2" src = "img/heart.svg">
+            <p class="text2"> ${maxHp} </p>
           </div>
           <div class="fourthRow">
             <div class="resistant">
-              <p class="text18">Resistencia</p>
+              <p class="text2">Resistencia</p>
               <div class="separar">${dividir(resistant)}</div>
             </div>
             <div class="weaknesses">
-              <p class="text18">Debilidad</p>
+              <p class="text2">Debilidad</p>
               <div class="separar">${dividir(weaknesses)}</div>
             </div>
           </div>
@@ -140,7 +141,7 @@ selectType.addEventListener('change', () => {
 
 // cons cambiarSeleccion = document.querySelector('#selecOrder');
 const cambiarSeleccion = () => {
-  const typeOrder = document.formulario.select[document.formulario.select.selectedIndex].value    document.formulario.select.value;
+  const typeOrder = document.formulario.select[document.formulario.select.selectedIndex].value;
   const cambioAZ = document.querySelector('#selectType');
   const pokemonOrden1 = ['Normal', 'Electric', 'Fighting', 'Flying', 'Poison', 'Ground',
     'Rock', 'Bug', 'Ghost', 'Steel', 'Fire', 'Water', 'Grass', 'Psychic', 'Ice', 'Dragon',
